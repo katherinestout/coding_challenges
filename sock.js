32ddaa172d0e64ce2fbe0dd74da5613171acdb72
katherinestout <kjs14@hood.edu>
2019-06-21 14:20:52 -0400

    solved compare approach

--- sock.js
+++ sock.js
@@ -1,30 +1,30 @@
 const n= 9;
 const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]; 
-pairs = '';
 
-// Complete the sockMerchant function below.
 function sockMerchant() {
-    //for each sock, match it to the ones that match, and then take them out from array
-    //add them up and put them in sorted array
 
 //first sort it
 //then say if the first i is equal to the next i, count goes up
 //if first i isnt equal to next i, get rid of first i then go to next i
+
+//sorted in ascending order
 let sorted = ar.sort((c, b) => c-b);
 console.log(sorted);
 
-for (i = 0; i < n; i++) {
-  // console.log(socks[i]);
-  var index = ar[i];
-  var a = ar[0];
+//final count
+let pairs = 0;
 
-  if (a === index){
-     // console.log(i);
-      pairs++;
-    
-  }  
+//looping over sorted array
+//comparing each item of array with direct sibling
+for (i = 0; i < n - 1; i++) {
+  if(sorted[i] === sorted[i + 1] ){
+    pairs ++;
+    //increment i by one, allows us to skip to next item
+    //if items don't match, normal loop continues
+    i += 1;
+  }
 }
 console.log(pairs);
-
+return pairs;
 }
 sockMerchant();
