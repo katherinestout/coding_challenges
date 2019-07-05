5978075d845b9ed3c513fbbc177cabd9d3b86db0
katherinestout <kjs14@hood.edu>
2019-07-05 14:26:56 -0400

    class methods

--- oop/index.js
+++ oop/index.js
@@ -19,8 +19,17 @@ class User {
         this.email = email;
         this.name = name;
     }
+    login(){
+        console.log(this.email, "logged in");
+    }
+    logout(){
+        console.log(this.email, "logged out");
+    }
 }
 
 //'new' keyword creates new empty object & sets value of 'this' to new empty object
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('kat@ninjas.com', 'Kat');
+
+userOne.login();
+userTwo.logout();
