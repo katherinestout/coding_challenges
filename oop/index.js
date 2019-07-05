27b218fb91fb838a68e675f457840ab0460b5378
katherinestout <kjs14@hood.edu>
2019-07-05 14:21:57 -0400

    classes

--- oop/index.js
+++ oop/index.js
@@ -1,4 +1,4 @@
-var userOne = {
+/* var userOne = {
 
     email: "kat@gmail.com",
     name: "kat",
@@ -10,4 +10,17 @@ var userOne = {
     }
 };
 
-console.log(userOne.email);
+console.log(userOne.email); */
+
+//class
+//constructor function
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+//'new' keyword creates new empty object & sets value of 'this' to new empty object
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('kat@ninjas.com', 'Kat');
