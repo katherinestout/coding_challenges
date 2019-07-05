78b9dd57284c46876cf1eccf722f8376576a8923
katherinestout <kjs14@hood.edu>
2019-07-05 15:05:42 -0400

    prototype

--- oop/index.js
+++ oop/index.js
@@ -70,9 +70,15 @@ function User(){
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = function(){
-        console.log(this.email, 'has logged in');
-    }
+}
+//the constructor function above has prototype property
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, "has logged in");
+}
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email, "has logged out");
 }
 
 var userOne = new User('one@gmail.com', 'one');
